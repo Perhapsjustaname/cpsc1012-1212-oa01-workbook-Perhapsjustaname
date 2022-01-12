54fe235899a4246d286550d420e42205e7730eca
Hamza Khawaja <mkhawaja1@nait.ca>
2022-01-12 16:49:45 -0700

    jan 12 2022

--- SaleApp/SaleApp/Program.cs
+++ SaleApp/SaleApp/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*  Purpose: computer the total of a sales item using the price and tax
+ 
+    Inputs: Price  in dollars
+ 
+    Outputs: Sales total 
+
+    Algorithm: Step 1) Prompt and read in the sale price
+               Step 2) Calculate the sale total using the formula:
+                      sale total = price * tax
+               Step 3) Display the computed sale total
+Last Modified: Jan 12, 2022
+ */ 
+
+using System;
 
 namespace SaleApp
 {
@@ -7,12 +20,13 @@ namespace SaleApp
         static void Main(string[] args)
         {
             double price,
-                tax,
-                total;
+                   tax,
+                   total;
 
-            price = 29.75;
-            tax = 1.76;
-            total = 31.51;
+            Console.WriteLine("Please enter the sale price: ");
+            price = double.Parse(Console.ReadLine());
+            tax = (price*1.05)-price;
+            total = price + tax;
 
             Console.WriteLine($"The price of the item is {price:C}");
             Console.WriteLine($"The tax of the item is {tax:C}");
---
+++ WagesApp/WagesApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WagesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                // read user input for basePay, regularHours, overtimePay, overTimeHours
+                Console.WriteLine("Enter your regular hours worked: ");
+                double regularHours = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your base pay :$ ");
+                double basePay = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your overtime hours worked: ");
+                double overtimeHours = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your overtime pay :$ ");
+                double overtimePay = double.Parse(Console.ReadLine());
+
+                //calculate total wages
+
+                double regularWages = basePay * regularHours;
+                double overtimeWages = overtimePay * overtimeHours;
+                double totalWages = regularWages + overtimeWages;
+                
+                //display the total wage
+                
+                Console.WriteLine("Wages for this week are $" + totalWages);
+              
+            }
+            
+        }
+    }
+}
